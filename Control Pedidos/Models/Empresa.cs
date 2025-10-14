10fa745dc65284f4bb499383888acc6161450467
DANIEL RIVERA HERRADA <108365290+danielriverah@users.noreply.github.com>
2025-10-14 10:59:07 -0600

    Replace nullable reference syntax for .NET Framework

---
+++ Control Pedidos/Models/Empresa.cs
@@ -0,0 +1,9 @@
+namespace Control_Pedidos.Models
+{
+    public class Empresa
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Rfc { get; set; } = string.Empty;
+    }
+}
